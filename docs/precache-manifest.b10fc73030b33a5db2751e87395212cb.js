76bdd690f1d96c661a54d6066d4c2e3c15ddad34
Daniel Ortega <daniel.ortega@ids.com.mx>
2020-08-15 22:30:09 -0500

    make build

---
+++ docs/precache-manifest.b10fc73030b33a5db2751e87395212cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864fc6e417c094bf77ccfd3c683756e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4092722ff4f7e5a99c1e",
+    "url": "/static/css/main.e487fedc.chunk.css"
+  },
+  {
+    "revision": "5741fdeb827e523eab2e",
+    "url": "/static/js/2.7a64fc32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a64fc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4092722ff4f7e5a99c1e",
+    "url": "/static/js/main.41353a12.chunk.js"
+  },
+  {
+    "revision": "abcc377b2955bd9a0d00",
+    "url": "/static/js/runtime-main.90d225dc.js"
+  },
+  {
+    "revision": "305bf5e1c8042a0abe03247d3e5c207a",
+    "url": "/static/media/perfil.305bf5e1.png"
+  },
+  {
+    "revision": "5f889adce431dd2954267f0fb83f8ce1",
+    "url": "/static/media/skill.5f889adc.jpg"
+  },
+  {
+    "revision": "274c8ed2d658416adec9fcf268c55920",
+    "url": "/static/media/work2.274c8ed2.jpg"
+  },
+  {
+    "revision": "68132e2f4a95f460583c529da8fd064d",
+    "url": "/static/media/work4.68132e2f.jpg"
+  },
+  {
+    "revision": "a38751a4e4d86b97a5d5fc2b5ed41706",
+    "url": "/static/media/work6.a38751a4.jpg"
+  }
+]);
